5986a6ec55f3a0fe1174631c1978a99855a6d732
Oleg Sinelnik <olegsinelnik@gmail.com>
2019-09-30 23:48:29 +0300

    Show Weather By City Name

--- script.js
+++ script.js
@@ -53,4 +53,67 @@ document.addEventListener("DOMContentLoaded", function() {
 				</p>`
 		})
 	}
+
+	var button = document.querySelector('.resetButton');
+	var input = document.querySelector('.text');
+
+	function showWeatherByCityName() {
+		var city = input.value;
+		city = city.charAt(0).toUpperCase() + city.substring(1, city.length).toLowerCase();
+		var requestWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=b76db1f799bfd7055d8b133c6667219c`);
+		requestWeather.then(objPromise => objPromise.json()).then(weatherData => {
+				if (localStorage.getItem('cityList') === null) {
+					var list = [];
+					list.push(weatherData);
+					localStorage.setItem('cityList', JSON.stringify(list));
+				} else {
+					var list = JSON.parse(localStorage.getItem('cityList'));
+					list.push(weatherData);
+					localStorage.setItem('cityList', JSON.stringify(list));
+				}
+				input.value = ''; // очищает поле ввода
+				showLocalStor();
+		}).catch(e => {
+			var list = JSON.parse(localStorage.getItem('cityList'));
+			list.pop();
+			localStorage.setItem('cityList', JSON.stringify(list));
+			input.setAttribute('placeholder', 'Not Found');
+			});
+	}
+
+	function refreshPlaceholderbyinput() {
+		input.setAttribute('placeholder', 'Enter the city...');
+	}
+
+	input.addEventListener('click', refreshPlaceholderbyinput);
+
+	button.addEventListener('click', showWeatherByCityName);
+	// input.addEventListener('change', showWeatherByCityName);
+
+
+	function showLocalStor() {
+		p.innerHTML = ' '; //очистить чтобы при отображении не дублировалась ранее выведенная инф.
+		if (localStorage.getItem('cityList') !== null) {
+			var list = JSON.parse(localStorage.getItem('cityList'));
+			for(var i = 0; i < list.length; i++) {
+				var weatherWidgetByCity = document.createElement('div');
+				p.appendChild(weatherWidgetByCity);
+				// weatherWidgetByCity.style.backgroundColor = '#5996cc';
+				weatherWidgetByCity.style.borderRadius = '10px';
+				weatherWidgetByCity.style.fontWeight = 'bold';
+				weatherWidgetByCity.style.textAlign = 'center';
+				weatherWidgetByCity.innerHTML = 
+					`<span class="city">${list[i].name}</span></br>
+					<span>temperature - ${Math.round(list[i].main.temp-273)}℃</span></br>
+					<span>humidity - ${list[i].main.humidity}%</span></br>
+					<span>wind - ${list[i].wind.speed}m/s</span></br>
+					<span>${list[i].weather[0].description}</span></br>
+					<img src="https://openweathermap.org/img/w/${list[i].weather[0].icon}.png" />`
+			}
+		}
+	}
+
+	showLocalStor();
+
+	
 });
